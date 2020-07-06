91c02c6be05047bc89420c96688ef6d9e5f76fa1
Virus <vladprodan6@gmail.com>
2020-07-06 22:25:33 +0300

    Updates

---
+++ precache-manifest.ea330219a7cd8377b40b9c5a5391e00a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51987983d6c97b47bcf75e5ccdc6a809",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "8e0d00c48807eb8d41f4",
+    "url": "/react-app/static/css/main.a0bf2a4b.chunk.css"
+  },
+  {
+    "revision": "100c61946e8d391df1b4",
+    "url": "/react-app/static/js/2.e2ff1e2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.e2ff1e2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e0d00c48807eb8d41f4",
+    "url": "/react-app/static/js/main.ad39959d.chunk.js"
+  },
+  {
+    "revision": "e7dd6fe6def1a575d179",
+    "url": "/react-app/static/js/runtime-main.5c7568c3.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/react-app/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/react-app/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/react-app/static/media/spinner.235b9b3e.gif"
+  }
+]);
